b2dd4c098fca766c1df43630377a0e83ea012b39
thenewjapzzz <guilherme.yokota20@gmail.com>
2025-02-13 01:23:35 -0300

    repeat the exercises in module arrays&strings

--- array&strings/exercises/contains-duplicate2.py
+++ array&strings/exercises/contains-duplicate2.py
@@ -1,13 +1,15 @@
-class Solution():
+class Solution(object):
     def containsNearbyDuplicate(self, nums, k):
         d = {}
-        for i, num in enumerate(nums):
-            if num in d and i - d[num] <= k:
+        for idx, num in enumerate(nums):
+            if num in d and idx - d[num] <= k:
                 return True
-            d[num] = i
+            d[num] = idx
+        
         return False
 
 solution = Solution()
 print(solution.containsNearbyDuplicate([1, 2, 3, 1], 3))
 print(solution.containsNearbyDuplicate([1, 0, 1, 1], 1))
 print(solution.containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2))
+        
