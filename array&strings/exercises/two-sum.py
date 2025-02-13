facc88c85841e44d35ed860a21205dd5bcf47e3b
thenewjapzzz <guilherme.yokota20@gmail.com>
2025-02-13 14:47:44 -0300

    testing autosave

--- array&strings/exercises/two-sum.py
+++ array&strings/exercises/two-sum.py
@@ -3,11 +3,11 @@ class Solution(object):
         d = {}
         for idx, num in enumerate(nums):
             if num in d:
-                return[d[num], idx] 
+                return [d[num], idx]
             d[target - num] = idx
 
+
 solution = Solution()
-print(solution.twoSum([2, 7,11, 15], 9))
+print(solution.twoSum([2, 7, 11, 15], 9))
 print(solution.twoSum([3, 2, 4], 6))
 print(solution.twoSum([3, 3], 6))
-        
